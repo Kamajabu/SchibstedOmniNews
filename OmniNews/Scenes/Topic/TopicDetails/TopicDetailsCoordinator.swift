4bf5511803b3590f9d6ea885d92351c7996a3171
Kamil Buczel <kamil.buczel@kamajabu.pl>
2019-10-08 01:00:23 +0200

    TopicDetailsCoordinator added
    Responsible for starting topicDetailsViewController

---
+++ OmniNews/Scenes/Topic/TopicDetails/TopicDetailsCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  TopicDetailsCoordinator.swift
+//  OmniNews
+//
+//  Created by Kamil Buczel on 07/10/2019.
+//  Copyright © 2019 Kamajabu. All rights reserved.
+//
+
+import UIKit
+
+class TopicDetailsCoordinator: Coordinator {
+    private let presenter: UINavigationController
+    private var topicDetailsViewController: TopicDetailsViewController?
+    private let model: Topic
+
+    init(presenter: UINavigationController, topic: Topic) {
+        self.presenter = presenter
+        model = topic
+    }
+
+    func start() {
+        let topicDetailsViewController = TopicDetailsViewController(model: model)
+        presenter.pushViewController(topicDetailsViewController, animated: true)
+        
+        self.topicDetailsViewController = topicDetailsViewController
+    }
+}

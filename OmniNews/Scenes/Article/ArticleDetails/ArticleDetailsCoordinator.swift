1065bbb9de47230eb36e4dd4093c9c982df7baff
Kamil Buczel <kamil.buczel@kamajabu.pl>
2019-10-08 00:58:06 +0200

    ArticleDetailsCoordinator added
    Responsible for displaying articlesDetailsViewController

---
+++ OmniNews/Scenes/Article/ArticleDetails/ArticleDetailsCoordinator.swift
@@ -0,0 +1,26 @@
+//
+//  ArticleDetailsCoordinator.swift
+//  OmniNews
+//
+//  Created by Kamil Buczel on 07/10/2019.
+//  Copyright © 2019 Kamajabu. All rights reserved.
+//
+
+import UIKit
+
+class ArticleDetailsCoordinator: Coordinator {
+    private var articlesDetailsViewController: ArticlesDetailsViewController?
+
+    private let presenter: UINavigationController
+    private let viewModel: ArticleViewModel
+
+    init(presenter: UINavigationController, article: ArticleViewModel) {
+        self.presenter = presenter
+        viewModel = article
+    }
+
+    func start() {
+        let articlesDetailsViewController = ArticlesDetailsViewController(viewModel: viewModel)
+        presenter.pushViewController(articlesDetailsViewController, animated: true)
+    }
+}

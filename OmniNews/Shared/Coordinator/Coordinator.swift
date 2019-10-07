2d593345c1429f3ed087bfde9dfb879149b7fc26
Kamil Buczel <kamil.buczel@kamajabu.pl>
2019-10-08 00:48:44 +0200

    Basic Coordinator protocol added

---
+++ OmniNews/Shared/Coordinator/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  OmniNews
+//
+//  Created by Kamil Buczel on 07/10/2019.
+//  Copyright © 2019 Kamajabu. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinator {
+    func start()
+}

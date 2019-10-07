6ff86f9d0e014fe865a5108a8c65cccff30da72f
Kamil Buczel <kamil.buczel@kamajabu.pl>
2019-10-08 00:54:03 +0200

    Names of images moved to SharedImages struct
    These are shared configuration

---
+++ OmniNews/Shared/Configuration/SharedImages.swift
@@ -0,0 +1,20 @@
+//
+//  SharedImages.swift
+//  OmniNews
+//
+//  Created by Kamil Buczel on 07/10/2019.
+//  Copyright © 2019 Kamajabu. All rights reserved.
+//
+
+import Foundation
+
+struct SharedImages {
+    static let unexpectedErrorImage = "thatsweird"
+    static let placeholder = "placeholder"
+
+    static let articlesMenuOff = "articles"
+    static let articlesMenuOn = "articles_on"
+
+    static let topicsMenuOff = "topics"
+    static let topicsMenuOn = "topics_on"
+}

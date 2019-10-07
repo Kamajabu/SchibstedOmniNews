739dac1a27dbd827e51619e148d4f1864bf8afd0
Kamil Buczel <kamil.buczel@kamajabu.pl>
2019-10-08 00:25:39 +0200

    NetworkKeys which are constants for firebase and server added

---
+++ OmniNews/Shared/Service/NetworkKeys.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkKeys.swift
+//  OmniNews
+//
+//  Created by Kamil Buczel on 08/10/2019.
+//  Copyright © 2019 Kamajabu. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkKeys {
+    // MARK: Firebase keys
+
+    static let articles = "articles"
+    static let topics = "topics"
+
+    // MARK: General network keys
+
+    static let baseUrl = "gfx-ios.omni.se"
+    static let images = "/images"
+}
